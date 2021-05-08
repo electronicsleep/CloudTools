398239bb54b0f4c9f1e016c954d9c4f3c36a97e6
Chris <electronicsleep@gmail.com>
2021-05-07 20:28:54 -0700

    cloud-tools-typer-example

--- ct/ct.py
+++ ct/ct.py
@@ -5,6 +5,7 @@ import pprint
 # Usage
 # python3 ct.py aws list-inst
 # python3 ct.py gcp list-inst --verbose
+# python3 ct.py endpoint-check http://www.chrisgr.com --verbose
 
 app = typer.Typer()
 
@@ -35,6 +36,19 @@ def gcp(cmd: str, verbose: bool = False):
     else:
         typer.echo(f"Run GPC cmd {cmd}")
 
+
+@app.command()
+def endpoint_check(url: str, verbose: bool = False):
+    """ Endpoint check """
+    typer.echo(f"Endpoint check {url}")
+    if verbose:
+        typer.echo(f"Verbose on")
+    response = requests.get(url)
+    if response.status_code == 200:
+        print("200 ok")
+    if verbose:
+        pprint.pprint(response.content)
+        pprint.pprint(response.text)
 
 if __name__ == "__main__":
     app()
