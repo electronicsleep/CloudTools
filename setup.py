bcaa1c4b01d26aa5e1f71869e53a0153c743b01d
Chris <electronicsleep@gmail.com>
2022-10-22 10:39:29 -0700

    update-rust-file-argparse-exampe

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from setuptools import setup
+#from distutils.core import setup
 
 setup(name='ct',
-      version='1.2',
-      py_modules=['ct', "ct_lib", "ct_inv"],
+      version='1.3',
       description='CloudTools - Example Python/Rust Tools using Typer/Fastapi',
+      py_modules=['ct', "ct_lib", "ct_inv", "ct_rust"],
+      exclude_package_data={"scripts": ["scripts"]},
       author='Chris https://github.com/electronicsleep',
       url='https://github.com/electronicsleep/CloudTools',
       install_requres=["typer==0.3.2", "typer-cli==0.0.11"],
---
+++ src/scripts/argparse_test.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# Author: electronicsleep@gmail.com
+# Purpose: Argparse for standalone scripts
+
+# Usage
+# python3 argparse_test.py -n test
+
+import time
+import argparse
+import sys
+
+# sys.path.append('../') # noqa
+# import ct_lib # noqa
+
+parser = argparse.ArgumentParser(description='CloudTools')
+required = parser.add_argument_group('required arguemets')
+optional = parser.add_argument_group('optional arguemets')
+required.add_argument('-n', '--name', help='Name', required=False)
+required.add_argument('-e', '--env', help='Env', required=False, default='qa')
+optional.add_argument('-r', '--region', help='Region', required=False, default='us-west-1')
+optional.add_argument('-t', '--test', help='Test', required=False, action='store_true')
+
+
+args = vars(parser.parse_args())
+name = args['name']
+env = args['env']
+region = args['region']
+test = args['test']
+
+time_now = time.strftime("%Y-%m-%d-%H%M%S")
+
+print(f"name: {name} env: {env} region {region} time_now: {time_now}") 
