0a4f35002c6526bbd1a08affc3a5b425455e40e4
Chris Robertson <electronicsleep@gmail.com>
2022-11-23 08:18:07 -0800

    adding-rich-colors

--- src/ct.py
+++ src/ct.py
@@ -9,6 +9,7 @@
 # python3 ct.py cs
 
 import typer
+from rich import print
 import ct_lib as ct_lib
 import ct_inv as ct_inv
 from pkg_resources import get_distribution
@@ -32,7 +33,7 @@ default_gcp_project = 'qa'
 
 def version_callback(value: bool):
     if value:
-        typer.echo(f"CloudTools (ct) Version: {__version__}")
+        print(f"CloudTools (ct) Version: {__version__}")
         raise typer.Exit()
 
 
@@ -47,25 +48,27 @@ def common(
 @main.command()
 def cs(verbose: bool = typer.Option(False, "--verbose", "-v")):
     """ Endpoint Check: Check Sites """
+    print("[bold red]check_sites[/bold red]")
     ct_lib.check_sites(ct_inv.server_list, verbose)
 
 
 @main.command()
 def test():
     """ Test cmd """
+    print("[bold red]test[/bold red]")
     ct_lib.ask_continue()
 
 
 @main.command()
-def aws(cmd: str = typer.Option(..., "--cmd", "-c", help="list-inst, list-rds, update-r53"),
+def aws(cmd: str = typer.Option("list-inst", "--cmd", "-c", help="list-inst, list-rds, update-r53"),
         verbose: bool = typer.Option(False, "--verbose", "-v")):
     """ aws sub cmd: default: list-inst """
     if verbose:
-        typer.echo(f"aws cmd: {cmd} verbose: {verbose}")
+        print(f"aws cmd: {cmd} verbose: {verbose}")
     if cmd == "list-rds":
-        typer.echo(f"aws cmd: {cmd}")
+        print(f"aws cmd: {cmd}")
     elif cmd == "update-r53":
-        typer.echo(f"aws cmd {cmd} are you sure you want to update r53?")
+        print(f"aws cmd {cmd} are you sure you want to update r53?")
         ct_lib.ask_continue()
         ct_lib.aws_update_r53(verbose)
     else:
@@ -74,11 +77,11 @@ def aws(cmd: str = typer.Option(..., "--cmd", "-c", help="list-inst, list-rds, u
 
 
 @main.command()
-def gcp(cmd: str = typer.Option(..., "--cmd", "-c", help="list-inst"),
+def gcp(cmd: str = typer.Option("list-inst", "--cmd", "-c", help="list-inst"),
         verbose: bool = typer.Option(False, "--verbose", "-v")):
     """ gcp sub cmd """
     if verbose:
-        typer.echo(f"gcp cmd: {cmd} verbose: {verbose}")
+        print(f"gcp cmd: {cmd} verbose: {verbose}")
     if cmd == "list-inst":
         ct_lib.gcp_list_inst(default_gcp_project, verbose)
     else:
@@ -91,12 +94,14 @@ if rust_support:
     @main.command()
     def rust_version():
         """ Rust Version """
+        print(f"[bold red]rust_version: {cmd}[/bold red]")
         ct_rust.rust_version()
 
     @main.command()
     def rust_print(cmd: str = typer.Option(..., "--cmd", "-c", help="list-inst"),
                    verbose: bool = typer.Option(False, "--verbose", "-v")):
         """ Rust Print """
+        print(f"[bold red]rust_print: {cmd}[/bold red]")
         ct_rust.rust_print(cmd, verbose)
 
     @main.command()
